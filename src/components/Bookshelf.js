c485cc8f7cc31daa39e71bd728a7082c6068713f
Russell Milburn <russell.milburn@team.telstra.com>
2018-06-30 10:17:17 +1000

    refactored: bookshelf is now a stateless component as it only displays books in the list and it does not change state

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -2,33 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
-class Bookshelf extends React.Component {
-  
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired,
-    optionsList: PropTypes.array.isRequired,
-    onUpdateBookshelf : PropTypes.func.isRequired
-  };
-  
-  render() {
-    const {title, books, optionsList, onUpdateBookshelf} = this.props;
-    
-    return (
-      <div>
-        <h2 className='bookshelf-title'>{title}</h2>
-        <ol className='books-grid'>
-          {books.map((book) => (
-            <li key={book.id}>
-              <Book book={book}
-                    optionsList={optionsList}
-                    onUpdateBookshelf={onUpdateBookshelf}/>
-            </li>
-          ))}
-        </ol>
-      </div>
-    )
-  }
-}
+const Bookshelf = ({title, books, optionsList, onUpdateBookshelf}) => {
+  return (
+    <div>
+      <h2 className='bookshelf-title'>{title}</h2>
+      <ol className='books-grid'>
+        {books.map((book) => (
+          <li key={book.id}>
+            <Book book={book}
+                  optionsList={optionsList}
+                  onUpdateBookshelf={onUpdateBookshelf}/>
+          </li>
+        ))}
+      </ol>
+    </div>
+  )
+};
+
+Bookshelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  optionsList: PropTypes.array.isRequired,
+  onUpdateBookshelf : PropTypes.func.isRequired
+};
 
 export default Bookshelf
